d89fae3fb0a88b83bee0a26d262d7f0b3c47ea99
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-06 17:28:15 +0300

    Second Commit

---
+++ primeNumberTesting.py
@@ -0,0 +1,14 @@
+def prime_number(n):
+
+
+    myList = []
+    if isinstance(n, int):
+        for i in range(n):
+            if n%i==0:
+                return "not prime"
+            else:
+                myLst.append(i)
+                print myList
+
+prime_number(6)
+                
