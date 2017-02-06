f43008b4ac82a0526dad2a54b76febaa81414d1c
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-06 19:52:46 +0300

    Improving on values les than six

--- prime_number.py
+++ prime_number.py
@@ -7,10 +7,10 @@ def prime_number(number):
             for i in range(2, number):
                 if i%2!=0 and i%3!=0 and i%5!=0:
                     lst.append(i)
-        elif number>1 and number<5:
+        elif number>1 and number<6:
             lst = [2,3,5]
         else:
-            return "Not prime number in range"
+            return "No prime number in range"
         return lst
     else:
         return "invalid input"
