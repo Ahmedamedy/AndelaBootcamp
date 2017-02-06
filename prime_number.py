afb50a300cbd393f0943d97f20b710a2a4cac226
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-06 18:57:28 +0300

    Checking for invalid input

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,15 @@
 def prime_number(number):
 
-    lst = []
-    for i in range(2, number):
-        if i%2!=0 and i%3!=0 and i%5!=0:
-            lst.append(i)
-    print lst
+    if isinstance(number, int):
+        lst = []
+        for i in range(2, number):
+            if i%2!=0 and i%3!=0 and i%5!=0:
+                lst.append(i)
+        print lst
+    else:
+        print "invalid input"
+
+prime_number(100)
 
             
         
