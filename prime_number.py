bcda5e133617abc5284e482b291f10d8d7dacc98
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-06 22:24:41 +0300

    Improving on squares

--- prime_number.py
+++ prime_number.py
@@ -2,18 +2,28 @@ def prime_number(number):
 
     if isinstance(number, int):
         lst = []
-        if number>5:
-            lst = [2,3,5]
+        if number>8:
+            lst = [2,3,5,7]
             for i in range(2, number):
-                if i%2!=0 and i%3!=0 and i%5!=0:
-                    lst.append(i)
+                if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
+                    if i % i**0.5 != 0:
+                        lst.append(i)
+        elif number>1 and number<3:
+            lst = [2]
+        elif number>1 and number<4:
+            lst = [2,3]
         elif number>1 and number<6:
             lst = [2,3,5]
+        elif number>1 and number<8:
+            lst = [2,3,5,7]
         else:
             return "No prime number in range"
         return lst
     else:
         return "invalid input"
+
+
+
 
             
         
