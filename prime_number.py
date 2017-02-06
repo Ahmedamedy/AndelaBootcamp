77e8c1ff0fad4b65a3e6c3450781bddf42df3979
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-06 19:09:37 +0300

    Checking for numbers less than 6

--- prime_number.py
+++ prime_number.py
@@ -2,9 +2,15 @@ def prime_number(number):
 
     if isinstance(number, int):
         lst = []
-        for i in range(2, number):
-            if i%2!=0 and i%3!=0 and i%5!=0:
-                lst.append(i)
+        if number>5:
+            lst = [2,3,5]
+            for i in range(2, number):
+                if i%2!=0 and i%3!=0 and i%5!=0:
+                    lst.append(i)
+        elif number>1 and number<5:
+            lst = [2,3,5]
+        else:
+            return "Not prime number in range"
         print lst
     else:
         print "invalid input"
